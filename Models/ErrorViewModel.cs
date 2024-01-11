5b8a197cb4ed3e202eabd71db791e913507cad21
pang <101445976@georgebrown.ca>
2024-01-10 21:25:56 -0500

    COMP2139-Wednesday

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace COMP2139_labs.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
