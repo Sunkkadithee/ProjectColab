944753c27cc0fee7a7e6697b9912a0fc2d7e1d9b
pang <101445976@georgebrown.ca>
2024-01-17 13:11:45 -0500

    Create ProjectController.cs

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using COMP2139_labs.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace COMP2139_labs.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            var projects = new List<Project>()
+            {
+                new Project { ProjectId = 1, Name = "Project 1", Description = " My First Project"}
+            };
+            return View(projects);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+    }
+}
+
