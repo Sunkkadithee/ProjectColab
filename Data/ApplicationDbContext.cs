30fd64317f3e93545befb7f269f95cd8507959d6
pang <101445976@georgebrown.ca>
2024-01-25 21:49:20 -0500

    Create ApplicationDbContext.cs

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using COMP2139_labs.Models;
+//using Microsoft.EntityFrameworkCore;
+
+
+namespace COMP2139_labs.Data
+{
+//	public class ApplicationDbContext : DBContext
+//	{
+	//	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
+
+		//public DbSet<Project> Projects { get; set; }
+	//}
+}
+
