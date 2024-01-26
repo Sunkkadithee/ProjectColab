6d4e12b9ffe6adda69f914f2803d0812d18ba79f
pang <101445976@georgebrown.ca>
2024-01-25 21:49:58 -0500

    Update project.cs

--- Models/project.cs
+++ Models/project.cs
@@ -5,15 +5,19 @@ namespace COMP2139_labs.Models
 {
 	public class Project
 	{
-		public int ProjectId { get; set; }
+
+        [Key]
+        public int ProjectId { get; set; }
 
         [Required]
         public required string Name { get; set; }
+
 
         public string? Description { get; set; }
 
         [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
+
         [DataType(DataType.Date)]
         public DateTime EndDate { get; set; }
 
