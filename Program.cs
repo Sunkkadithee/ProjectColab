81d081d04b2197c168e6fe5dfcb05ec911e24a4d
pang <101445976@georgebrown.ca>
2024-02-16 22:39:22 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,23 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using COMP2139_labs.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContextPool<ApplicationDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseStatusCodePagesWithRedirects("/Home/NotFound?statusCode={0}");
 }
 
 app.UseHttpsRedirection();
